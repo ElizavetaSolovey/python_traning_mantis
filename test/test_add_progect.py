8dc5ede2a7a31c4907048ca32d95c7867a739a4c
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-06-27 22:21:57 +0300

    Рефакторинг теста добавления проекта

--- test/test_add_progect.py
+++ test/test_add_progect.py
@@ -5,15 +5,16 @@ def test_add_project(app, db, json_projects):
     old_projects = app.soap.get_project_list()
     if len(old_projects) != 0:
         if app.project.is_project_with_name_present(old_projects, project):
-            app.project.create_project(project)
-            new_projects = app.soap.get_project_list()
-            old_projects.append(project)
-            assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+            assert_create_project(app, old_projects, project)
     else:
-        app.project.create_project(project)
-        new_projects = app.soap.get_project_list()
-        old_projects.append(project)
-        assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+        assert_create_project(app, old_projects, project)
+
+
+def assert_create_project(app, old_projects, project):
+    app.project.create_project(project)
+    new_projects = app.soap.get_project_list()
+    old_projects.append(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
 
 
 
